96d0037f6b5238fcab8778d362dae4bb9bdc7755
Milton839 <miltonbiswas839@gmail.com>
2021-03-30 15:53:09 +0600

    first commit

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import fakeData from '../../fakeData';
+
+const Inventory = () => {
+    const handleAddProduct = () => {
+        const product = {}
+        fetch('https://vast-headland-56097.herokuapp.com/addProduct',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(product)
+        })
+    }
+    return (
+        <div>
+            <form action="" method="post" class="mt-2">
+                <p><span>Name: </span><input type="text"/></p>
+                <p><span>Price: </span><input type="text"/></p>
+                <p><span>Quantity: </span><input type="text"/></p>
+                <p><span>Product Image: </span><input type="file"/></p>
+                <button onClick={handleAddProduct}>Add Product</button>
+            </form>
+        </div>
+    );
+};
+
+export default Inventory;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCgxZEarStVej3Z5eGUTW83KGaaH0IAPt8",
+  authDomain: "ema-john-with-database.firebaseapp.com",
+  projectId: "ema-john-with-database",
+  storageBucket: "ema-john-with-database.appspot.com",
+  messagingSenderId: "265977460059",
+  appId: "1:265977460059:web:07df51908b175e12ae967e"
+};
+export default firebaseConfig;
